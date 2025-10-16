e9f0a9be4069fcae64e70fdcd22d3a8eed0f43c6
majing <majing@clouditera.com>
2025-10-16 18:06:31 +0800

    fix:解决使用xinference服务报错，修改文档翻译并发数

--- pdf2zh_next/gui.py
+++ pdf2zh_next/gui.py
@@ -1856,6 +1856,7 @@ with gr.Blocks(
                 output_file_glossary,
                 output_title,  # Visibility of output title
             ],
+            concurrency_limit=5,
         )
 
         # Cancel button click handler
